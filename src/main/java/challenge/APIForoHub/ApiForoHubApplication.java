05c8fcb0bf7bd3beee3d4631cc8d65ff514c0963
Natalia Otarola <nat.otarola@gmail.com>
2025-01-11 20:11:59 -0300

    Initial commit

---
+++ src/main/java/challenge/APIForoHub/ApiForoHubApplication.java
@@ -0,0 +1,13 @@
+package challenge.APIForoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/APIForoHub/ApiForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.APIForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
